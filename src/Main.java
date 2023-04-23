d24e0d4b083d89cf4b9e84a0118c6206692a9681
omercohen35 <omer35cohen@gmail.com>
2023-04-23 11:53:18 +0300

    try new stuff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static Scanner scanner;
+    public static Random rnd;
+    public static int[][] initBoard() {
+        String[] n_m = scanner.nextLine().split("X");
+        int n = Integer.parseInt(n_m[0]);
+        int m = Integer.parseInt(n_m[1]);
+        int[][] board = new int[n][m];
+        System.out.println(board[4][2]);
+
+        return board;
+    }
+    public static void battleshipGame() {
+        int[][] board = initBoard();
+        System.out.println("Hello");
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        scanner = new Scanner(new File(path));
+        int numberOfGames = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Total of " + numberOfGames + " games.");
+
+        for (int i = 1; i <= numberOfGames; i++) {
+            scanner.nextLine();
+            int seed = scanner.nextInt();
+            rnd = new Random(seed);
+            scanner.nextLine();
+            System.out.println("Game number " + i + " starts.");
+            battleshipGame();
+            System.out.println("Game number " + i + " is over.");
+            System.out.println("------------------------------------------------------------");
+        }
+        System.out.println("All games are over.");
     }
 }
+
+
+
